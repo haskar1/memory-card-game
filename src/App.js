6a06e1914ba6a638a982cdee573f921358a90de7
Hussein Askar <94493175+haskar1@users.noreply.github.com>
2023-06-25 02:03:03 -0400

    Add logo and instructions

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import Game from "./components/Game.js";
+import logoImg from "./img/logo.png";
 
 function App() {
   return (
     <div>
+      <header className="ff-pokemon-solid">
+        <img className="logo-img" src={logoImg} alt="Pokemon logo" />
+        <p className="logo-instructions">Don't click the same pokemon twice!</p>
+      </header>
+      <div className="logo-black-line">
+        <div className="logo-big-circle">
+          <div className="logo-small-circle"></div>
+        </div>
+      </div>
       <main>
         <Game />
       </main>
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles.css';
+import './fonts/Pokemon-Hollow.ttf';
+import './fonts/Pokemon-Solid.ttf'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
